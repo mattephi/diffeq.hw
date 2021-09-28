ba0251eb9a80a28c8f2181fcdfceb51add5611a7
Igor Alentev <m8dotpie@Igors-MBP.Dlink>
2021-09-28 10:53:23 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DE_Plot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DE_Plot.dir/main.cpp.o"
+  "DE_Plot"
+  "DE_Plot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DE_Plot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

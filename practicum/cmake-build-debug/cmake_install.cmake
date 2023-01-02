8fd68fa3cda2a485277ff1faf9835a0b5dc11347
Igor Alentev <alentev.olm@ya.ru>
2023-01-02 17:33:40 +0300

    Update BUILD

--- practicum/cmake-build-debug/cmake_install.cmake
+++ practicum/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/m8dotpie/Desktop/Projects/DE_Plot
+# Install script for directory: /Users/m8dotpie/Projects/diffeq.hw/practicum
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/m8dotpie/Desktop/Projects/DE_Plot/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/m8dotpie/Projects/diffeq.hw/practicum/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
